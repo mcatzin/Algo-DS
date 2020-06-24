45fdde8c97b240fd3f2afbd1f9860512858d3b1a
Mario Catzin <catzin21@gmail.com>
2020-06-24 15:13:46 -0800

    search

---
+++ search.java
@@ -0,0 +1,26 @@
+/*
+Given a sorted (in ascending order) integer array nums of n elements and a target value,
+ write a function to search target in nums. 
+If target exists, then return its index, otherwise return -1.
+
+*/
+
+public class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
