e35c61bed27e57a0dcad66400577a954f0737d17
Mario Catzin <catzin21@gmail.com>
2020-06-24 07:59:37 -0800

    remove elements

---
+++ removeElements.java
@@ -0,0 +1,28 @@
+/*
+Remove all elements from a linked list of integers that have value val.
+Remove all elements from a linked list of integers that have value val.
+*/
+/**
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode() {} ListNode(int val) { this.val = val; } ListNode(int val,
+ * ListNode next) { this.val = val; this.next = next; } }
+ */
+
+public class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+
+        while (prev.next != null) {
+            if (prev.next.val == val) {
+                prev.next = prev.next.next;
+            } else {
+                prev = prev.next;
+            }
+        }
+
+        return dummy.next;
+
+    }
+}
