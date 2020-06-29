2babea0f7a7e3930461f6717aad933ed06ef9e01
Mario Catzin <catzin21@gmail.com>
2020-06-29 07:30:23 -0800

    find disappear number in python

---
+++ findDisappearedNumbersPython.py
@@ -0,0 +1,29 @@
+"""
+Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
+Find all the elements of [1, n] inclusive that do not appear in this array.
+
+Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+
+Example:
+
+Input:
+[4,3,2,7,8,2,3,1]
+
+Output:
+[5,6]
+
+"""
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            temp = abs(nums[i]) - 1
+            if nums[temp] > 0:
+                nums[temp] *= -1
+        res = []
+
+        for i, n in enumerate(nums):
+            if n > 0:
+                res.append(i + 1)
+
+        return res
