e8ab6496c0e2b672e0cc0f56c4df2c3004083697
Mario Catzin <catzin21@gmail.com>
2020-06-28 22:20:39 -0800

    exist dfs O(nm4^s)

---
+++ exist79.py
@@ -0,0 +1,45 @@
+"""
+
+Given a 2D board and a word, find if the word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+Example:
+
+board =
+[
+  ['A','B','C','E'],
+  ['S','F','C','S'],
+  ['A','D','E','E']
+]
+
+Given word = "ABCCED", return true.
+Given word = "SEE", return true.
+Given word = "ABCB", return false.
+"""
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        visited = {}
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if self.getWords(board, word, i, j, visited):
+                    return True
+        
+        return False
+
+    def getWords(self, board, word, i, j, visited, pos = 0):
+        if pos == len(word):
+            return True
+
+        if i < 0 or i == len(board) or j < 0 or j == len(board[0]) or visited.get((i, j)) or word[pos] != board[i][j]:
+            return False
+
+        visited[(i, j)] = True
+        res = self.getWords(board, word, i, j + 1, visited, pos + 1) \
+                or self.getWords(board, word, i, j - 1, visited, pos + 1) \
+                or self.getWords(board, word, i + 1, j, visited, pos + 1) \
+                or self.getWords(board, word, i - 1, j, visited, pos + 1)
+        visited[(i, j)] = False
+
+        return res
