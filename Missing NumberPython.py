45863c1b78ba1121cdf08ac1fd974c0f253b7c7c
Mario Catzin <catzin21@gmail.com>
2020-06-29 07:13:10 -0800

    missing number in python

---
+++ Missing NumberPython.py
@@ -0,0 +1,18 @@
+"""
+Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+
+Example 1:
+
+Input: [3,0,1]
+Output: 2
+"""
+
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        
+        return n*(n + 1) /2 - sum(nums)
