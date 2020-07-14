a81b330115be357653425d9031d20960b305df03
Mario Catzin <catzin21@gmail.com>
2020-07-13 21:49:35 -0700

    finde duplicate js

--- productExceptSelfjs.js
+++ productExceptSelfjs.js
@@ -8,19 +8,19 @@ Output: [24,12,8,6]
 */
 
 var productExceptSelf = function (nums) {
-    let result = [];
-    result.unshift(1);
+  let result = [];
+  result.unshift(1);
 
-    for (let i = 1; i < nums.length; i++) {
-        result[i] = result[i - 1] * nums[i - 1];
-    }
+  for (let i = 1; i < nums.length; i++) {
+    result[i] = result[i - 1] * nums[i - 1];
+  }
 
-    let right = 1;
+  let right = 1;
 
-    for (let i = nums.length - 1; i >= 0; i--) {
-        result[i] *= right;
-        right *= nums[i];
-    }
+  for (let i = nums.length - 1; i >= 0; i--) {
+    result[i] *= right;
+    right *= nums[i];
+  }
 
-    return result;
+  return result;
 };
