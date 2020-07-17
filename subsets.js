dd3085e3adade4bab541d407c1582269f40482ca
Mario Catzin <catzin21@gmail.com>
2020-07-16 23:35:45 -0700

    sunsets backtracking

---
+++ subsets.js
@@ -0,0 +1,42 @@
+/*
+Given a set of distinct integers, nums, return all possible subsets (the power set).
+
+Note: The solution set must not contain duplicate subsets.
+
+Example:
+
+Input: nums = [1,2,3]
+Output:
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+*/
+var subsets = function (nums) {
+    let result = [
+        []
+    ];
+
+    function backtrack(first, current) {
+
+        for (let i = first; i < nums.length; i++) {
+            current.push(nums[i])
+
+            result.push([...current])
+
+            backtrack(i + 1, current);
+
+            current.pop();
+        }
+    }
+
+    backtrack(0, []);
+
+    return result;
+};
